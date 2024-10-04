793190a1a1827884fb9e9d26615ff511c3ebb750
Adetona Mustapha <adetona3005@gmail.com>
2024-10-04 04:17:24 +0100

    task

--- 0x07-react_redux_action_creator_normalizr/task_0/dashboard/src/schema/notifications.js
+++
@@ -1,7 +0,0 @@
-import * as notifications from '../../../../notifications.json';
-
-const getAllNotificationsByUser = (userId) => 
-  notifications.default.filter((notification) => notification.author.id === userId)
-                .map((notification) => notification.context);
-
-export default getAllNotificationsByUser;
--- 0x09-react_redux_connectors_and_providers/task_9/dashboard/src/selectors/courseSelector.js
+++
@@ -1,6 +0,0 @@
-export const courseSelector = (state) => {
-  const courses = state.courses;
-
-  if (courses) return courses.valueSeq();
-  return courses;
-};
